00d57a8c8bc0f50caf851944f769d5841493dbeb
吴子豪 <vortez.wohl@gmail.com>
2025-09-08 20:50:06 +0800

    feat: 支持提示词转markdown格式

--- prompt4py/template/general_template.py
+++ prompt4py/template/general_template.py
@@ -1,39 +1,14 @@
 import json
 
 from prompt4py.template.base_template import BaseTemplate
+from prompt4py.util.json2markdown import json_to_markdown
 
 
 class GeneralTemplate(BaseTemplate):
     def __init__(self):
         super().__init__()
 
-    def render_system(self):
-        _prompt_object = {
-            'role': self.role,
-            'objective': self.objective,
-            'constraint': self.constraint,
-            'capability': self.capability,
-            'output_datatype': self.output_dtype,
-            'output_format': self.output_format,
-            'output_language': self.output_language,
-            'output_example': self.output_example
-        }
-        for k in list(_prompt_object.keys()):
-            if len(str(_prompt_object[k])) < 1:
-                del _prompt_object[k]
-        return json.dumps(_prompt_object, ensure_ascii=False)
-
-    def render_user(self):
-        _prompt_object = {
-            'instruction': self.instruction,
-            'context': self.context
-        }
-        for k in list(_prompt_object.keys()):
-            if len(str(_prompt_object[k])) < 1:
-                del _prompt_object[k]
-        return json.dumps(_prompt_object, ensure_ascii=False)
-
-    def render(self) -> str:
+    def render(self, parse_markdown: bool = False) -> str:
         _prompt_object = {
             'role': self.role,
             'objective': self.objective,
@@ -49,4 +24,6 @@ class GeneralTemplate(BaseTemplate):
         for k in list(_prompt_object.keys()):
             if len(str(_prompt_object[k])) < 1:
                 del _prompt_object[k]
+        if parse_markdown:
+            return json_to_markdown(json_data=_prompt_object)
         return json.dumps(_prompt_object, ensure_ascii=False)
