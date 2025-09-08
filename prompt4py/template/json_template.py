d28122c765f611eef02761db8a7689c29deb2b22
吴子豪 <vortez.wohl@gmail.com>
2025-09-08 19:39:30 +0800

    feat: 实现json提示词模板

--- prompt4py/template/json_template.py
+++ prompt4py/template/json_template.py
@@ -7,6 +7,32 @@ class JsonTemplate(BaseTemplate):
     def __init__(self):
         super().__init__()
 
+    def render_system(self):
+        _prompt_object = {
+            'role': self.role,
+            'objective': self.objective,
+            'constraint': self.constraint,
+            'capability': self.capability,
+            'output_datatype': self.output_dtype,
+            'output_format': self.output_format,
+            'output_language': self.output_language,
+            'output_example': self.output_example
+        }
+        for k in list(_prompt_object.keys()):
+            if len(str(_prompt_object[k])) < 1:
+                del _prompt_object[k]
+        return json.dumps(_prompt_object, ensure_ascii=False)
+
+    def render_user(self):
+        _prompt_object = {
+            'instruction': self.instruction,
+            'context': self.context
+        }
+        for k in list(_prompt_object.keys()):
+            if len(str(_prompt_object[k])) < 1:
+                del _prompt_object[k]
+        return json.dumps(_prompt_object, ensure_ascii=False)
+
     def render(self) -> str:
         _prompt_object = {
             'role': self.role,
